c501b5b2914ea0ca07d3216ea2fd898ce93d2c51
Jorge Pasco <jorge.pasco@oktana.io>
2020-06-11 15:53:13 -0500

    fix recordIdField values in XMD

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/analytics_icon" {
+    var analytics_icon: string;
+    export default analytics_icon;
+}
+declare module "@salesforce/resourceUrl/einstein_scene" {
+    var einstein_scene: string;
+    export default einstein_scene;
+}
