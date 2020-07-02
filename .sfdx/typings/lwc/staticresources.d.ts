e5770725a3db57ef77e1f237e0a964a5c41c92de
ttse-sfdc <ttse@salesforce.com>
2020-07-02 12:57:03 -0700

    Removed .sfdx folder

--- .sfdx/typings/lwc/staticresources.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@salesforce/resourceUrl/analytics_icon" {
-    var analytics_icon: string;
-    export default analytics_icon;
-}
-declare module "@salesforce/resourceUrl/einstein_scene" {
-    var einstein_scene: string;
-    export default einstein_scene;
-}
